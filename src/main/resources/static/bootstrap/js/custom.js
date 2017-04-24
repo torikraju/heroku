03207ea57164e2ebc661ee449757c9fd662c8d29
torikraju <torikraju@hotmail.com>
2017-04-25 04:59:38 +0600

    Notice related work

--- src/main/resources/static/bootstrap/js/custom.js
+++ src/main/resources/static/bootstrap/js/custom.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
 
-	$( "#registration" ).validate({
-		errorClass: 'text-danger',  
-		rules: {
-		    password: "required",
-		    c_passwd: {
-		      equalTo: "#password"
-		    }
-		  }
-		});
+	// registration form validation
+	$("#registration").validate({
+		errorClass : 'text-danger',
+		rules : {
+			password : "required",
+			c_passwd : {
+				equalTo : "#password"
+			}
+		}
+	});
+	console.log(jQuery().jquery);
+	
+	
 
 });
+
